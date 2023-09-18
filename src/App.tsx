3595d763eaaa08327b7ca5befc1b897bb211ae4f
Barbara-nunes <babisnunes14@gmail.com>
2023-09-18 19:41:28 -0300

    Enviando projeto para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+
+import styles from './App.module.css';
+
+import {ITask} from './interfaces/Task'
+
 
 function App() {
+
+  const [ taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Header />
+
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm 
+            btnText="Criar Tarefa" 
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        </div>
+
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
+      </main>
+
+      <Footer />  
+    </div> 
   );
 }
 
